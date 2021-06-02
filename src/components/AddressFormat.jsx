2c2854e708ec1f2959ad92c19c5fcaecbe6e34dc
arisac <hi@aris.ac>
2021-06-02 03:58:52 -0700

    comp: AddressFormat add 0x0 addr

--- src/components/AddressFormat.jsx
+++ src/components/AddressFormat.jsx
@@ -1,5 +1,8 @@
 export function AddressFormat(address = '') {
   let displayAddress = address
+  if (displayAddress === '0x0000000000000000000000000000000000000000') {
+    return '0x0'
+  }
   displayAddress = displayAddress.substr(0, 6) + '···' + displayAddress.substr(-4)
   return displayAddress
 }
