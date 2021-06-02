2e11e8dbb3c795e724ad2713ee834f7f919cdf36
arisac <hi@aris.ac>
2021-06-02 03:59:25 -0700

    add transfers to Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,8 +3,11 @@ import { useQuery, QueryClient, QueryClientProvider } from 'react-query'
 import { request, gql } from 'graphql-request'
 import NftListCard from 'components/NftListCard'
 import { Link } from 'react-router-dom'
+import { AddressFormat } from 'components/AddressFormat'
+import { RelativeTime, DateTime } from 'components/DateTime'
 
 const endpoint = process.env.REACT_APP_API_ENDPOINT
+const endpoint2 = process.env.REACT_APP_API_ENDPOINT_2
 const queryClient = new QueryClient()
 
 function Home() {
@@ -19,6 +22,7 @@ function Home() {
         <main>
           <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <Stats />
+            <Transfers />
             <Tokens />
           </div>
         </main>
@@ -60,21 +64,68 @@ function useGetTokens() {
 }
 
 function useGetStats() {
-  return useQuery('Stats', async () => {
-    const { stats } = await request(
-      endpoint,
-      gql`
-        query {
-          stats: alls {
-            numTokens
-            numOwners
-            numTokenContracts
+  return useQuery(
+    'Stats',
+    async () => {
+      const { stats } = await request(
+        endpoint,
+        gql`
+          query {
+            stats: alls {
+              numTokens
+              numOwners
+              numTokenContracts
+            }
           }
-        }
-      `
-    )
-    return stats
-  })
+        `
+      )
+      return stats
+    },
+    {
+      refetchInterval: 3000
+    }
+  )
+}
+
+function useGetTransfers() {
+  return useQuery(
+    'Transfers',
+    async () => {
+      const { transfers } = await request(
+        endpoint2,
+        gql`
+          query {
+            transfers(orderBy: timestamp, orderDirection: desc, first: 10) {
+              id
+              from {
+                id
+              }
+              to {
+                id
+              }
+              token {
+                tokenID
+                registry {
+                  id
+                  name
+                  symbol
+                }
+              }
+              transaction {
+                id
+                blockNumber
+                timestamp
+              }
+            }
+          }
+        `
+      )
+      return transfers
+    },
+    {
+      refetchInterval: 1000
+    }
+  )
 }
 
 function Tokens() {
@@ -82,7 +133,7 @@ function Tokens() {
 
   return (
     <div>
-      <h1>Tokens</h1>
+      <h1>New Tokens</h1>
       <Link to="/all">View All NFTs</Link>
       <div>
         {status === 'loading' ? (
@@ -109,14 +160,14 @@ function Stats() {
 
   return (
     <div>
-      <h1>Stats</h1>
+      <h1>Stats {isFetching ? 'Updating...' : ''}</h1>
       {status === 'loading' ? (
         'Loading...'
       ) : status === 'error' ? (
         <span>Error: {error.message}</span>
       ) : (
         <>
-          {data.map((stats, index) => (
+          {data.map(stats => (
             <div>
               <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                 <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
@@ -136,9 +187,66 @@ function Stats() {
               </dl>
             </div>
           ))}
-
-          {isFetching ? 'Background Updating...' : ''}
         </>
+      )}
+    </div>
+  )
+}
+
+function Transfers() {
+  const { status, data, error, isFetching } = useGetTransfers()
+
+  return (
+    <div>
+      <h1>Latest Transfers {isFetching ? 'Updating...' : ''}</h1>
+
+      {status === 'loading' ? (
+        'Loading...'
+      ) : status === 'error' ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <div class="flex flex-col">
+          <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                  <thead>
+                    <tr>
+                      <th>Token Contract</th>
+                      <th>TokenID</th>
+                      <th>From</th>
+                      <th>To</th>
+                      <th>Time</th>
+                      <th>Txn Hash</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.map((transfer, index) => (
+                      <tr key={index}>
+                        <td title={transfer.token.registry.id}>
+                          {transfer.token.registry.name} ({transfer.token.registry.symbol})
+                        </td>
+                        <td>
+                          <Link to={'/view/' + transfer.token.registry.id + '/' + transfer.token.tokenID}>
+                            {transfer.token.tokenID}
+                          </Link>
+                        </td>
+                        <td>{AddressFormat(transfer.from.id)}</td>
+                        <td>{AddressFormat(transfer.to.id)}</td>
+                        <td title={DateTime(transfer.transaction.timestamp * 1000)}>
+                          {RelativeTime(transfer.transaction.timestamp * 1000)}
+                        </td>
+                        <td title={transfer.transaction.id + '(' + 'block:' + transfer.id + ')'}>
+                          {AddressFormat(transfer.transaction.id)}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   )
