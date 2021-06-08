bbdc4e4eaa3f7686fe2c0d7601e1f8b0fe3017a6
arisac <hi@aris.ac>
2021-06-08 02:29:12 -0700

    update viewer

--- src/components/NftSingle.jsx
+++ src/components/NftSingle.jsx
@@ -8,16 +8,17 @@ import { SearchIcon } from '@heroicons/react/outline'
 
 const EXPLORER_BASE_URL = process.env.REACT_APP_EXPLORER_URL
 const CHAIN_ID = process.env.REACT_APP_NETWORK_CHAINID
+let layoutNumber = 0
 
 const NftSingle = props => {
   const token = props.token
   const [tokenName, setTokenName] = useState(null)
   const [tokenImage, setTokenImage] = useState(null)
   const [tokenDescription, setTokenDescription] = useState(null)
-  const [layoutType, setLayoutType] = useState(null)
-  const [layoutCount, setLayoutCount] = useState(0)
+  const [layoutType, setLayoutType] = useState('layout-post')
 
   useEffect(() => {
+    layoutNumber = 0
     if (!token) {
       return
     }
@@ -25,43 +26,57 @@ const NftSingle = props => {
     const getTokenMetaData = async () => {
       if (GetUriProtocol(token.tokenURI) === 'http') {
         setTokenName('Blocked From Accessing Insecure HTTP NFT Content')
-        setTokenImage('Blocked From Accessing Insecure HTTP NFT Content')
-        setTokenDescription('Blocked From Accessing Insecure HTTP NFT Content')
+        setLayoutType('layout-raw-address')
         return
       }
 
       const tokenMetaData = await getMetaData(UriResolver(token.tokenURI))
 
-      if (!tokenMetaData.name || !tokenMetaData.image) {
-        setTokenName('no name')
-        setTokenImage('no image')
+      if (!tokenMetaData.name || tokenMetaData.name === '') {
+        setTokenName('')
       } else {
         setTokenName(tokenMetaData.name)
+        layoutNumber = layoutNumber + 1
+      }
+
+      if (!tokenMetaData.description || tokenMetaData.description === '') {
+        setTokenDescription('')
+      } else {
+        setTokenDescription(
+          tokenMetaData.description.split(/\n+|<br \/>+|<br\/>+|<br>+/).map((str, index) => <p key={index}>{str}</p>)
+        )
+        layoutNumber = layoutNumber + 2
+      }
+
+      if (!tokenMetaData.image || tokenMetaData.image === '') {
+        setTokenImage('')
+      } else {
         const imgUrl = UriResolver(tokenMetaData.image)
         setTokenImage(<img src={imgUrl} alt="" />)
+        layoutNumber = layoutNumber + 4
       }
-      if (!tokenMetaData.description) {
-        setTokenDescription('no description')
-      } else {
-        setTokenDescription(tokenMetaData.description)
-      }
-    }
-    getTokenMetaData()
 
-    switch (layoutCount) {
-      case 0:
-        setLayoutType('layout-raw-address')
-        break
-      case 1:
-        setLayoutType('layout-big-title')
-        break
-      case 4:
-        setLayoutType('layout-media-cover')
-        break
-      default:
-        // setLayoutType('layout-post')
-        setLayoutType('layout-raw-address')
+      switch (layoutNumber) {
+        case 0:
+          setLayoutType('layout-raw-address')
+          setTokenName(CHAIN_ID + ':' + token.contract.id + '/' + token.tokenID)
+          break
+        case 1:
+          setLayoutType('layout-big-title')
+          break
+        case 2:
+          setLayoutType('layout-no-title')
+          break
+        case 4:
+          setLayoutType('layout-media-cover')
+          break
+        default:
+          setLayoutType('layout-post')
+          break
+      }
     }
+
+    getTokenMetaData()
   }, [token])
 
   return (
@@ -78,7 +93,7 @@ const NftSingle = props => {
                   {token.contract.name} <span>{token.tokenID}</span>
                 </div>
                 <div className="addr">
-                  <span alt={token.contract.id}>{AddressFormat(token.contract.id)}</span>
+                  <span alt={token.contract.id}>{CHAIN_ID + ':'}<span className="mono">{AddressFormat(token.contract.id)}</span></span>
                 </div>
               </div>
             </div>
@@ -91,12 +106,12 @@ const NftSingle = props => {
             </dl>
             <dl>
               <dt>Created By</dt>
-              <dd>{AddressFormat(token.minter)}</dd>
+              <dd className='mono'>{AddressFormat(token.minter)}</dd>
             </dl>
             <dl></dl>
             <dl>
               <dt>Current Holder</dt>
-              <dd>{AddressFormat(token.owner.id)}</dd>
+              <dd className='mono'>{AddressFormat(token.owner.id)}</dd>
             </dl>
           </section>
 
@@ -112,31 +127,44 @@ const NftSingle = props => {
             <h4>Chain Info</h4>
             <dl>
               <dt>Contract Address</dt>
-              <dd>{token.contract.id}</dd>
+              <dd className='mono'>{token.contract.id}</dd>
             </dl>
             <div>
               <dl>
+                <dt>Token Name</dt>
+                <dd>{token.contract.name}</dd>
+              </dl>
+              <dl>
+                <dt>Token Symbol</dt>
+                <dd>{token.contract.symbol}</dd>
+              </dl>
+            </div>
+            <div>
+              <dl>
                 <dt>Token ID</dt>
-                <dd>{token.tokenID}</dd>
+                <dd className='mono'>{token.tokenID}</dd>
               </dl>
               <dl>
                 <dt>Created On Block</dt>
-                <dd>{token.mintBlock}</dd>
+                <dd className='mono'>{token.mintBlock}</dd>
               </dl>
             </div>
             <dl>
               <dt>Token URI</dt>
-              <dd>{token.tokenURI}</dd>
+              <dd className='mono'>{token.tokenURI}</dd>
             </dl>
           </section>
 
           <footer>
-            <div hidden>
-              <SearchIcon className="w-10 h-10" />
+            <div>
+              <a
+                href={EXPLORER_BASE_URL + 'tokens/' + token.contract.id + '/instance/' + token.tokenID}
+                className="button"
+              >
+                <SearchIcon />
+                <p>View In Explorer</p>
+              </a>
             </div>
-            <a href={EXPLORER_BASE_URL + 'tokens/' + token.contract.id + '/instance/' + token.tokenID}>
-              View In Explorer
-            </a>
           </footer>
         </div>
       </div>
