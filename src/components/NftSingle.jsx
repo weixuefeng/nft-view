1470f66de0e62eb2bd3b0fbb7baae51edcccc792
arisac <hi@aris.ac>
2021-06-07 23:34:24 -0700

    update viewer

--- src/components/NftSingle.jsx
+++ src/components/NftSingle.jsx
@@ -3,6 +3,8 @@ import { UriResolver, GetUriProtocol } from 'components/UriResolver'
 import { getMetaData } from 'components/GetMetaData'
 import { AddressFormat } from 'components/AddressFormat'
 import { DateTime } from 'components/DateTime'
+import { NewtonCoinIcon } from 'components/icons'
+import { SearchIcon } from '@heroicons/react/outline'
 
 const EXPLORER_BASE_URL = process.env.REACT_APP_EXPLORER_URL
 const CHAIN_ID = process.env.REACT_APP_NETWORK_CHAINID
@@ -12,6 +14,8 @@ const NftSingle = props => {
   const [tokenName, setTokenName] = useState(null)
   const [tokenImage, setTokenImage] = useState(null)
   const [tokenDescription, setTokenDescription] = useState(null)
+  const [layoutType, setLayoutType] = useState(null)
+  const [layoutCount, setLayoutCount] = useState(0)
 
   useEffect(() => {
     if (!token) {
@@ -43,49 +47,100 @@ const NftSingle = props => {
       }
     }
     getTokenMetaData()
+
+    switch (layoutCount) {
+      case 0:
+        setLayoutType('layout-raw-address')
+        break
+      case 1:
+        setLayoutType('layout-big-title')
+        break
+      case 4:
+        setLayoutType('layout-media-cover')
+        break
+      default:
+        // setLayoutType('layout-post')
+        setLayoutType('layout-raw-address')
+    }
   }, [token])
 
   return (
-    <div>
-      <header>
-        <div>
-          <div>{token.contract.symbol}</div>
-          <div>
-            <div>
-              {token.contract.name} <span>{token.tokenID}</span>
+    <>
+      <div className={'viewer ' + layoutType}>
+        <div className="wrapper">
+          <header>
+            <div className="collection">
+              <div className="logo">
+                <NewtonCoinIcon />
+              </div>
+              <div className="info">
+                <div className="contract">
+                  {token.contract.name} <span>{token.tokenID}</span>
+                </div>
+                <div className="addr">
+                  <span alt={token.contract.id}>{AddressFormat(token.contract.id)}</span>
+                </div>
+              </div>
             </div>
+          </header>
+
+          <section className="token-basic">
+            <dl>
+              <dt>Created On</dt>
+              <dd>{DateTime(token.mintTime * 1000)}</dd>
+            </dl>
+            <dl>
+              <dt>Created By</dt>
+              <dd>{AddressFormat(token.minter)}</dd>
+            </dl>
+            <dl></dl>
+            <dl>
+              <dt>Current Holder</dt>
+              <dd>{AddressFormat(token.owner.id)}</dd>
+            </dl>
+          </section>
+
+          <main>
+            <h1>{tokenName}</h1>
+
+            <div className="cover">{tokenImage}</div>
+
+            <div className="content">{tokenDescription}</div>
+          </main>
+
+          <section className="chain-data">
+            <h4>Chain Info</h4>
+            <dl>
+              <dt>Contract Address</dt>
+              <dd>{token.contract.id}</dd>
+            </dl>
             <div>
-              <span alt="{token.contract.id}">{AddressFormat(token.contract.id)}</span>
+              <dl>
+                <dt>Token ID</dt>
+                <dd>{token.tokenID}</dd>
+              </dl>
+              <dl>
+                <dt>Created On Block</dt>
+                <dd>{token.mintBlock}</dd>
+              </dl>
             </div>
-          </div>
+            <dl>
+              <dt>Token URI</dt>
+              <dd>{token.tokenURI}</dd>
+            </dl>
+          </section>
+
+          <footer>
+            <div hidden>
+              <SearchIcon className="w-10 h-10" />
+            </div>
+            <a href={EXPLORER_BASE_URL + 'tokens/' + token.contract.id + '/instance/' + token.tokenID}>
+              View In Explorer
+            </a>
+          </footer>
         </div>
-      </header>
-
-      <div>{tokenImage}</div>
-
-      <div>
-        <h3>{tokenName}</h3>
-        <dl>
-          <dd>
-            <span className="font-mono">
-              <p>{tokenDescription}</p>
-              <p>Creator: {AddressFormat(token.minter)}</p>
-              <p>Created On: {DateTime(token.mintTime * 1000)}</p>
-              <p>Owner: {AddressFormat(token.owner.id)}</p>
-            </span>
-          </dd>
-        </dl>
       </div>
-
-      <div>
-        <h1 className="font-bold">Chain Info</h1>
-        <a href={EXPLORER_BASE_URL + 'tokens/' + token.contract.id + '/instance/' + token.tokenID}>View In Explorer</a>
-        <p>Chain ID: {CHAIN_ID}</p>
-        <p>Contract Address: {token.contract.id}</p>
-        <p>Token ID: {token.tokenID}</p>
-        <p>Created On Block: #{token.mintBlock}</p>
-      </div>
-    </div>
+    </>
   )
 }
 
