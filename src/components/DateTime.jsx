b7eb46c3a906ee329c956088ab1c41dea6997f0e
arisac <hi@aris.ac>
2021-06-14 23:15:32 -0700

    update DateTime

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -4,9 +4,9 @@ import moment from 'moment'
 
 // Convert timestamp to YYYY-MM-DD HH:MM:SS
 export function DateTime(timestamp = Date.now()) {
-  return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
+  return moment(timestamp).format('YYYY-MM-DD HH:mm:ss UTCZZ').slice(0, -2)
 }
 
 export function RelativeTime(timestamp = Date.now()) {
-  return moment(timestamp).fromNow()
+  return moment(timestamp).fromNow(true)
 }
