b567d4eaeecdaca774bcee7063a5438a8dd90220
arisac <hi@aris.ac>
2021-06-22 10:53:44 -0700

    update comp: DateTime() & RelativeTime()

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -3,10 +3,16 @@ import moment from 'moment'
 // timestamp using unix timestamp in milliseconds
 
 // Convert timestamp to YYYY-MM-DD HH:MM:SS
-export function DateTime(timestamp = Date.now()) {
-  return moment(timestamp).format('YYYY-MM-DD HH:mm:ss UTCZZ').slice(0, -2)
+export function DateTime(timestamp = Date.now(), timezone = false) {
+  if (timezone) {
+    return moment(timestamp).format('YYYY-MM-DD HH:mm:ss UTCZZ').slice(0, -2)
+  }
+  return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
 }
 
-export function RelativeTime(timestamp = Date.now()) {
+export function RelativeTime(timestamp = Date.now(), suffix = true) {
+  if (suffix) {
+    return moment(timestamp).fromNow()
+  }
   return moment(timestamp).fromNow(true)
 }
